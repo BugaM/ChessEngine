5ea73ac882bfcd526145a033638f6bffef5c2e75
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2021-07-21 18:07:57 -0300

    Change values from pawns to centipawns.

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 import chess
-# Evaluation Constants
-piece_value = {chess.PAWN: 1, chess.KNIGHT: 3, chess.BISHOP: 3, chess.ROOK: 5, chess.QUEEN: 9, chess.KING: 0}
-outcome_value = {chess.Termination.CHECKMATE: 1000, chess.Termination.STALEMATE : 0, chess.Termination.FIFTY_MOVES: 0,
+# Evaluation Constants (in centipawns)
+piece_value = {chess.PAWN: 100, chess.KNIGHT: 300, chess.BISHOP: 300, chess.ROOK: 500, chess.QUEEN: 900, chess.KING: 0}
+outcome_value = {chess.Termination.CHECKMATE: 100000, chess.Termination.STALEMATE : 0, chess.Termination.FIFTY_MOVES: 0,
                 chess.Termination.FIVEFOLD_REPETITION: 0, chess.Termination.INSUFFICIENT_MATERIAL: 0, chess.Termination.SEVENTYFIVE_MOVES: 0}
 # 
 EPSILON = 10e-3
