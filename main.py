070760f2d70613d5d315240bd05dcb8bf046538e
buga <marcelo.buga@hotmail.com>
2021-07-23 22:38:50 -0300

    making alpha beta minimax

--- main.py
+++ main.py
@@ -23,5 +23,6 @@ def start_game():
         game_input(board, screen)
     pygame.quit()
 
+
 if __name__ == "__main__":
         start_game()
--- utils.py
+++ utils.py
@@ -1,8 +1,6 @@
 from constants import WINDOW_HEIGHT, WINDOW_LENGTH, CHESS_BOARD_SIZE, SQUARE_SIZE
-import random
 import chess
 import game_logic
-import move_selection
 
 board_position = ((WINDOW_LENGTH-CHESS_BOARD_SIZE)//2, (WINDOW_HEIGHT-CHESS_BOARD_SIZE)//2)
 
@@ -31,47 +29,9 @@ def mouse_in_chessboard(mouse_position):
             and board_position[1] <= mouse_position[1] <= board_position[1] + CHESS_BOARD_SIZE)
 
 
-def random_player(board):
-    move = random.choice(list(board.legal_moves))
-    return move.uci()
-
-
-def alpha_beta_player(board):
-    return move_selection.alfa_beta_prunning(board, game_logic.MATERIAL_EVAL, game_logic.max_depth).uci()
-
-
-def stockfish_greedy(board):
-    move = move_selection.stockfish_move(board, game_logic.MATERIAL_EVAL, game_logic.max_depth)
-    return move.uci()
-
 
 def who(player):
     return "White" if player == chess.WHITE else "Black"
 
 
-def display_board(board, use_svg):
-    if use_svg:
-        return board._repr_svg_()
-    else:
-        return "<pre>" + str(board) + "</pre>"
 
-
-def human_player(board):
-    display(board)
-    uci = get_move("%s's move [q to quit]> " % who(board.turn))
-    legal_uci_moves = [move.uci() for move in board.legal_moves]
-    while uci not in legal_uci_moves:
-        print("Legal moves: " + (",".join(sorted(legal_uci_moves))))
-        uci = get_move("%s's move[q to quit]> " % who(board.turn))
-    return uci
-
-
-def get_move(prompt):
-    uci = input(prompt)
-    if uci and uci[0] == "q":
-        raise KeyboardInterrupt()
-    try:
-        chess.Move.from_uci(uci)
-    except:
-        uci = None
-    return uci
